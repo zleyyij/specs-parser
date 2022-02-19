977989d16d956cc8282225c23d379b1bf4e3e1fc
zleyyij <zleyyij@stu.ccsdut.org>
2022-02-19 11:13:11 -0700

    for you potato

--- index.js
+++ index.js
@@ -1,67 +1,70 @@
 console.log("Starting...");
-const axios = require('axios');
 
-var response;
+//loading dependancies
+import axios from 'axios';
+import { countReset } from 'console';
+import fs from 'fs';
+import { parse } from 'node-html-parser';
+// object where all data is stored
 let hw={};
+//raw response.data from paste
+let response;
+//parsed html data
+ let root;
+ //innerHTML for root
+ let core;
 
-function stripTdTags(input){
-  return input.replace("</td><td>", " ");
+//Niceties functions
 
 
-}
-
-
+//Obtaining the raw results from the paste
 async function getSpecsResults(){
+  response = await axios.get("https://paste.rtech.support/selif/8s8sc55j.html");
+  response = await response.data;
+  root = parse(await response);
+  core = await parse(root.innerHTML).rawText;
+  }
 
-let specsResponse = await axios.get("https://paste.rtech.support/selif/5cy6avnh.html").catch(function (error) {
-    if (error.response) {
-      // Request made and server responded
-      console.log(error.response.data);
-      console.log(error.response.status);
-      console.log(error.response.headers);
-    } else if (error.request) {
-      // The request was made but no response was received
-      console.log(error.request);
-    } else {
-      // Something happened in setting up the request that triggered an Error
-      console.log('Error', error.message);
-    }
-
-  });
-response = await specsResponse.data;
-
-}
-
-
+  
+  //Regexing issues from the text
 async function getIssues(){
-  console.log(await response.match(/(?<=<h2>Visible issues<\/h2>)(.*)(?=<a name="top"><\/a>)/gis));
-
+  hw.issues = core.match(/(?<=Visible issues\r\n)(.*)(?=\r\nSections)/gis)[0];
 }
+
+
 async function hwSummary(){
-  //regexing the cpu from the summary and removing residual html tags
-  hw.cpu = stripTdTags(response.match(/(?<=<tr><td>CPU<\/td><td>)(.*)(?=z<\/td>)[z]/gis)[0]);
-  console.log(hw.cpu);
-  hw.gpu = stripTdTags(response.match(/(?<=Video Card<\/td><td>(NVIDIA|AMD|INTEL)<\/td><td>)(.*)(?=<\/td><td>( OHMF|[0-9][0-9]))/gi)[0])
-  console.log(hw.gpu);
-  console.log(response.match(/(?<=RAM<\/th><\/tr>\n)(.*)(?=<\/tbody><\/table>\n<table>\n<colgroup><col><col><col><col><col><col><\/colgroup>)/gis));
-  hw.ram = stripTdTags(response.match(/(?<=RAM<\/th><\/tr>\n)(.*)(?=<\/tbody><\/table>\n<table>\n<colgroup><col><col><col><col><col><col><\/colgroup>)/gis)[0])
-  console.log(hw.ram)
-//working string
-///(?<=Video Card<\/td><td>(NVIDIA|AMD|INTEL)<\/td><td>)(.*)(?=<\/td><td>( OHMF)(<\/td><\/tr>))/gis
-  
-  
-
-  // dirtyResponse = response.match(/(?<=<tr><td>CPU<\/td><td>)(.*)(?=z<\/td>)[z]/gis)
- // console.log(dirtyResponse + `\n` + cleanResponse);
-
+//obtaining cpu data from core
+if(core.match(/(?<=CPU(GenuineIntel|AuthenticAMD|HygonGenuine))(.*)(?=( OHMF\r\nMotherboard|[0-9][0-9]\r\n Motherboard|\r\n [0-9][0-9][0-9]\n Motherboard\n))/gis)){
+hw.cpu = core.match(/(?<=CPU(GenuineIntel|AuthenticAMD|HygonGenuine))(.*)(?=( OHMF\r\nMotherboard|[0-9][0-9]\r\n Motherboard|\r\n [0-9][0-9][0-9]\n Motherboard\n))/gis)[0];
+} else{
+hw.cpu = 'Not Found'
 }
-async function main(){
-    
-    
-        await getSpecsResults();
-        await getIssues();
-        await hwSummary();
+//motherboard
+if(core.match(/(?<=Motherboard)(.*)(?=Video Card)/gis)){
+  hw.mobo = core.match(/(?<=Motherboard)(.*)(?=\r\nVideo Card)/gis)[0];
+} else {
+  hw.mobo= 'Not Found'
+}
+//gpu
+if(core.match(/(?<=Video Card)(.*)(?=( OHMF|[0-9][0-9]\r\nTotalRAM))/gis)){
+  hw.gpu = core.match(/(?<=Video Card(NVIDIA|AMD|INTEL))(.*)(?=( OHMF|[0-9][0-9]\r\nTotalRAM))/gis)[0];
+} else hw.gpu = 'Not Found';
+//ram
+if(core.match(/(?<=TotalRam\r\n)(.*)(?=\r\n\r\n\r\n\r\nManuf)/gis)){
+ hw.ram = core.match(/(?<=TotalRam\r\n)(.*)(?=\r\n\r\n\r\n\r\nManuf)/gis)[0]
+}
+}
 
-        console.log("Program Ended.")
+
+async function main(){
+  //Setting response to data
+  await getSpecsResults();
+      await hwSummary();
+       await getIssues();
+       
+   
+       console.log(hw)
+
+    console.log("Program Ended.")
 }
 main();
